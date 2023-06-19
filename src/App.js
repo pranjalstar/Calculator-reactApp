d5fe593c35badf08fd56f426be0bb5b28f285e50
Pranjal Kumar Singh <112309658+pranjalstar@users.noreply.github.com>
2023-06-19 18:27:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [value, setValue]=useState('')
+
+
+  const back = () => {
+    setValue((prevValue) => prevValue.slice(0, -1));
+  };
+
+    return(
+    
+    <div className="container">
+       
+    <div className="calculator">
+       
+        <form action="">
+
+       <div className="display">
+      <input  type="text" value={value}/>
+       </div>
+
+       <div>
+        <input type="button" value="AC" onClick={e => setValue('')}  />
+           <input type="button" value="DE"  onClick={back}  />
+
+       
+        <input type="button" value="." onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="/" className="style" onClick={e => setValue(value+ e.target.value)}  />
+       </div>
+
+       <div>
+        <input type="button" value="7" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="8"  onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="9"  onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="*"  className="style" onClick={e => setValue(value+ e.target.value)}  />
+       </div>
+       <div>
+        <input type="button" value="4"  onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="5" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="6" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="+"  className="style" onClick={e => setValue(value+ e.target.value)}  />
+       </div>
+
+
+       <div>
+        <input type="button" value="1" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="2" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="3" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="-"  className="style" onClick={e => setValue(value+ e.target.value)}  />
+       </div>
+         
+
+       <div>
+        <input type="button" value="00" onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="0"  onClick={e => setValue(value+ e.target.value)}  />
+        <input type="button" value="="  className="equal" onClick={e => setValue(eval(value))}  />
+      
+       </div>
+
+
+        </form>
+      
+      </div>
+    
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+export default App
